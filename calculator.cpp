7fc89b06de6a82b63fdec2c63110c29dea3f56e5
unknown <bogdan0706042@gmail.com>
2024-10-13 14:15:40 +0300

    added comments

--- calculator.cpp
+++ calculator.cpp
@@ -3,14 +3,19 @@
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
+/додавання
 }
 
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
+/віднімання
 }
 
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
+/множення
+/text
+/text
 }
